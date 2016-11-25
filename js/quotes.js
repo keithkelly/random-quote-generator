462b027881cb160ebe79d77e1d3b85e7f282a2a4
Keith Kelly <keithkelly31@gmail.com>
2016-11-25 14:43:30 -0500

    Add Quotes
    Added a series of quotes

---
+++ js/quotes.js
@@ -0,0 +1,34 @@
+var quotes = [
+	{
+		quote: "I have not failed. I've just found 10,000 ways that won't work.",
+		source: 'Thomas A. Edison'
+	},
+	{
+		citation: 'Inaugural Address',
+		quote: "And so, my fellow Americans: ask not what your country can do for you – ask what you can do for your country.",
+		source: 'John F. Kennedy',
+		year: '1961'
+	},
+	{
+		citation: 'Special Message to Congress',
+		quote: "America was not built on fear. America was built on courage, on imagination and an unbeatable determination to do the job at hand.",
+		source: 'Harry S. Truman',
+		year: '1947'
+	},
+	{
+		citation: 'Farewell Address to the Nation',
+		quote: "All great change in America begins at the dinner table.",
+		source: 'Ronald Reagan',
+		year: '1989'
+	},
+	{
+		quote: "We are called the nation of inventors. And we are. We could still claim that title and wear its loftiest honors if we had stopped with the first thing we ever invented, which was human liberty.",
+		source: 'Mark Twain'
+	},
+	{
+		citation: 'Gettysburg Address',
+		quote: "This nation, under God, shall have a new birth of freedom; and that government of the people, by the people, for the people, shall not perish from the earth.",
+		source: 'Abraham Lincoln',
+		year: '1863'
+	}
+];
