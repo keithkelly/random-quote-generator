7e89c9bee1a8a08ed0e33a9ad92ab30e67beba0f
Keith Kelly <keithkelly31@gmail.com>
2016-11-26 09:31:16 -0500

    Added function to remove quotes
    Function removes quotes from array as they are displayed

--- js/script.js
+++ js/script.js
@@ -1,37 +1,53 @@
-// FUNCTION: Returns random RGB color for background
-function randomBackgroundColor() {
-	return 'rgb(' + randomNumber(256) + ',' + randomNumber(256) + ',' + randomNumber(256) + ')';
-}
-
-// FUNCTION: Returns random number between 0 and the limit (not including limit number)
+// Returns random number between 0 and the limit (not including limit number)
 function randomNumber(limit) {
 	return Math.floor(Math.random() * limit);
 } 
 
-// FUNCTION: Returns a random quote from the quotes array
-function getRandomQuote() {
-	var quoteNum = randomNumber(quotes.length);
-	return quotes[quoteNum];	
+// Returns random RGB color for background
+function randomBackgroundColor() {
+	return 'rgb(' + randomNumber(256) + ',' + randomNumber(256) + ',' + randomNumber(256) + ')';
 }
 
-// FUNCTION: Prints random quote generated from random quote function
+// Returns a random quote from the quotes array
+function getRandomQuote() {
+	var quoteIndex = randomNumber(quotes.length);
+	return quotes[quoteIndex];
+}
+
+// Removes quote object passed to it from array
+function removeQuote(quote) {
+	return quotes.splice(quotes.indexOf(quote), 1);
+}
+
+// Prints random quote generated from random quote function
 function printQuote() {
 	var getQuote = getRandomQuote();
-	var quote = '';
-	quote += '<p class="quote">' + getQuote.quote + '</p>';
-	quote += '<p class="source">' + getQuote.source; 
+	var printQuote = '';
 
+	printQuote += '<p class="quote">' + getQuote.quote + '</p>';
+	printQuote += '<p class="source">' + getQuote.source; 
+
+	// Only show citation if it exists
 	if( getQuote.citation ) {
-		quote += '<span class="citation">' + getQuote.citation + '</span>';
+		printQuote += '<span class="citation">' + getQuote.citation + '</span>';
 	}
 
+	// Only show year if it exists
 	if( getQuote.year ) {
-		quote += '<span class="year">' + getQuote.year + '</span>';
+		printQuote += '<span class="year">' + getQuote.year + '</span>';
 	}
 
-	quote += '</p>';
+	printQuote += '</p>';
+
+	// Change the background color
 	document.body.style.backgroundColor = randomBackgroundColor();
+
+	// Show quote on page
 	document.getElementById('quote-box').innerHTML = quote;
+
+	// Log quote to console then remove from array
+	console.log(getQuote);
+	removeQuote(getQuote);
 }
 
 // event listener to respond to "Show another quote" button clicks
