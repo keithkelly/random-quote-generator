96bc70be5173d1a1b5b43819d7f91c51692ecef1
Keith Kelly <keithkelly31@gmail.com>
2016-11-26 11:32:45 -0500

    Added additional properties to quotes

--- js/quotes.js
+++ js/quotes.js
@@ -1,32 +1,44 @@
 var quotes = [
 	{
+		accomplishments: 'Invented light bulb and phonograph',
+		occupation: 'Inventor',
 		quote: "I have not failed. I've just found 10,000 ways that won't work.",
 		source: 'Thomas A. Edison'
 	},
 	{
+		accomplishments: 'Established the Peace Corp',
 		citation: 'Inaugural Address',
+		occupation: 'President of the United States',
 		quote: "And so, my fellow Americans: ask not what your country can do for you – ask what you can do for your country.",
 		source: 'John F. Kennedy',
 		year: '1961'
 	},
 	{
+		accomplishments: 'Ended World War II in 1945',
 		citation: 'Special Message to Congress',
+		occupation: 'President of the United States',
 		quote: "America was not built on fear. America was built on courage, on imagination and an unbeatable determination to do the job at hand.",
 		source: 'Harry S. Truman',
 		year: '1947'
 	},
 	{
+		accomplishments: 'Ended cold war',
 		citation: 'Farewell Address to the Nation',
+		occupation: 'President of the United States',
 		quote: "All great change in America begins at the dinner table.",
 		source: 'Ronald Reagan',
 		year: '1989'
 	},
 	{
+		accomplishments: 'Wrote "Adventures of Tom Sawyer" and "Adventures of Huckleberry Finn"',
+		occupation: 'Author',
 		quote: "We are called the nation of inventors. And we are. We could still claim that title and wear its loftiest honors if we had stopped with the first thing we ever invented, which was human liberty.",
 		source: 'Mark Twain'
 	},
 	{
+		accomplishments: 'Issued Emancipation Proclamation ending slavery',
 		citation: 'Gettysburg Address',
+		occupation: 'President of the United States',
 		quote: "This nation, under God, shall have a new birth of freedom; and that government of the people, by the people, for the people, shall not perish from the earth.",
 		source: 'Abraham Lincoln',
 		year: '1863'
